171b761b08c2b278cee43370f9fca58850585fac
Alifya Zhafira <alifya.zhafira@ui.ac.id>
2022-11-18 06:13:44 +0700

    PT Modul 8

--- app/src/main/java/com/alifyaZhafiraJSleepJS/jsleep_android/MainActivity.java
+++ app/src/main/java/com/alifyaZhafiraJSleepJS/jsleep_android/MainActivity.java
@@ -1,39 +1,84 @@
 package com.alifyaZhafiraJSleepJS.jsleep_android;
-
-import static com.alifyaZhafiraJSleepJS.jsleep_android.Utils.getAssetJsonData;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.content.res.AssetManager;
-import android.os.Bundle;
-import android.util.Log;
+import android.content.Intent;
 import android.view.Menu;
-import android.view.MenuInflater;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.alifyaZhafiraJSleepJS.jsleep_android.model.Room;
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Type;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-import com.google.gson.*;
+import com.alifyaZhafiraJSleepJS.jsleep_android.model.Room;
 
-public class MainActivity<json> extends AppCompatActivity {
-
-    /*String data = getAssetJsonData(getApplicationContext());
-    Type type = new TypeToken<class>(){}.getType();
-    class properties = new Gson().fromJson(data, type);*/
-
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        InputStream filepath = null;
+        ArrayList<Room> ListRoom = new ArrayList<>();
+        ArrayList<String> listId = new ArrayList<>();
+        Gson gison = new Gson();
+
+        try {
+            filepath = getAssets().open("randomRoomList.json");
+            BufferedReader baca = new BufferedReader(new InputStreamReader(filepath));
+            Room[] buff = gison.fromJson(baca, Room[].class);
+            Collections.addAll(ListRoom, buff);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (Room r : ListRoom) {
+            listId.add(r.name);
+        }
+        ArrayAdapter<String> adapt = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listId);
+        ListView listView = findViewById(R.id.ListViewId);
+        listView.setAdapter(adapt);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.top_menu, menu);
-        return true;
+        getMenuInflater().inflate(R.menu.top_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
+    public String loadJSONFromAsset() {
+        try {
+            InputStream is = MainActivity.this.getAssets().open("randomRoomList.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            String json = new String(buffer, "UTF-8");
+            return json;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.person_button:
+                Intent inte = new Intent(MainActivity.this, AboutMe.class);
+                startActivity(inte);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
